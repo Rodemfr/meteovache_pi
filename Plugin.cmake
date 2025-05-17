dbab41d1b78762af86e37fcd20be8bc6ef128a7b
Rodemfr <ronan.demoment@free.fr>
2025-05-17 21:11:24 +0200

    Version 1.4.0

--- Plugin.cmake
+++ Plugin.cmake
@@ -32,7 +32,7 @@ set(OCPN_RELEASE_REPO
 # -------  Plugin setup --------
 #
 set(PKG_NAME meteovache_pi)
-set(PKG_VERSION  1.3.7.0)
+set(PKG_VERSION  1.4.0.0)
 set(PKG_PRERELEASE "")  # Empty, or a tag like 'beta'
 
 set(DISPLAY_NAME MeteoVache)    # Dialogs, installer artifacts, ...
